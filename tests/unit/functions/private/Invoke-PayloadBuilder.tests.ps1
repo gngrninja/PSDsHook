f5cde0b2684553664f72167b8ee0ed1c939375a3
gngrninja <thegngrninja@gmail.com>
2018-08-12 22:34:49 -0700

    Added tests for Invoke-PayloadBuilder

---
+++ tests/unit/functions/private/Invoke-PayloadBuilder.tests.ps1
@@ -0,0 +1,47 @@
+InModuleScope PsDsHook {
+
+    describe 'Invoke-PayloadBuilder' {
+
+        mock 'Invoke-RestMethod' {}
+
+        it 'Handles embeds properly' {
+
+            [System.Collections.ArrayList]$embedArray = @()
+            $thumbUrl                                 = 'https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png'
+            $embedBuilder                             = [DiscordEmbed]::New('title','description')
+
+            $embedBuilder.AddField(
+                [DiscordField]::New(
+                    'field name', 
+                    'field value/contents', 
+                    $true
+                )
+            )
+
+            $embedBuilder.AddField(
+                [DiscordField]::New(
+                    'field name2',
+                    'field value2/contents2', 
+                    $true
+                )
+            )
+
+            $embedBuilder.AddThumbnail(
+                [DiscordThumbnail]::New(
+                    $thumbUrl
+                )
+            )
+
+            $embedArray.Add($embedBuilder) | Out-Null
+            $embedArray.Add($embedBuilder) | Out-Null
+
+            $payload = Invoke-PayloadBuilder -PayloadObject $embedArray
+
+            $payload              | Should Not Be $null
+            $payload.embeds.Count | Should Be 2
+            $payload.embeds[0]    | Should Be $embedArray[0]
+            $payload.embeds[1]    | Should Be $embedArray[1]
+
+        }
+    }
+}

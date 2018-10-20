94f0d139b3aa54da5d6e5c0f883503f319ad1fda
gngrninja <thegngrninja@gmail.com>
2018-10-20 01:15:22 -0700

    !deploy

--- examples/SimpleMessage.ps1
+++ examples/SimpleMessage.ps1
@@ -1,9 +1,5 @@
 using module PSDsHook
 
-#If the module is not in one of the folders listed in ($env:PSModulePath -split "$([IO.Path]::PathSeparator)")
-#You must specify the full path to the psm1 file in the above using statement
-#Example: using module 'C:\users\thegn\repos\PsDsHook\out\PSDsHook\0.0.1\PSDsHook.psm1'
-
 #Message to send
 $message = "This is the message"
 
